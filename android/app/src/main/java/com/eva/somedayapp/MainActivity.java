2b471989f85552e9cd1058a4d64f8555992639f8
Eva Filipe <99880409+evaffsimoes@users.noreply.github.com>
2026-09-11 16:22:38 +0200

    Feat: Update app, icons and widgets

--- android/app/src/main/java/com/eva/somedayapp/MainActivity.java
+++ android/app/src/main/java/com/eva/somedayapp/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends BridgeActivity {
         if (prefsListener != null) prefs.unregisterOnSharedPreferenceChangeListener(prefsListener);
     }
 
-    private void handleShareIntent(Intent intent) {
+    private void handleShareIntent(Itent intent) {
         if (intent == null) return;
         String action = intent.getAction();
         String type = intent.getType();
