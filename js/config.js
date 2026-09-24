cfa063fb649cc25f693b0dd55688dc2fde9f1bde
Eva Filipe <99880409+evaffsimoes@users.noreply.github.com>
2026-09-25 00:42:42 +0200

    Update js/config.js with correct Firebase API Key AIzaSyDo1Ky9P0cH2KIWErXti0Lzzj3a8uNrVzo

--- js/config.js
+++ js/config.js
@@ -3,12 +3,12 @@
  */
 
 window.CUE_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBUgrcixsC5W0shIt3IvvuYoqibASQ_pF4",
+  apiKey: "AIzaSyDo1Ky9P0cH2KIWErXti0Lzzj3a8uNrVzo",
   authDomain: "cue-events-21ac9.firebaseapp.com",
   databaseURL: "https://cue-events-21ac9-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "cue-events-21ac9",
   storageBucket: "cue-events-21ac9.firebasestorage.app",
   messagingSenderId: "87973671324",
-  appId: "1:87973671324:web:face01e87135d4e153d859",
+  appId: "1:87973671324:android:8ee151b36003166b53d859",
   measurementId: "G-H3Q4JG3FX6"
 };
